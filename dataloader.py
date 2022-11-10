9e2061a29fa92eb90ed1a0270a78c02952394ea3
charan250498 <charan2598@gmail.com>
2022-11-10 00:47:35 -0800

    Added CIFAR DataLoaders and training loop code.

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,18 @@
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import transforms
+from torchvision import datasets
+
+# Batch Size
+batch_size = 4
 
 # define necessary transforms
-transform = ...
+transform = transforms.Compose([transforms.ToTensor()])
 
 # Define dataset class or object of CIFAR
+trainset = datasets.CIFAR10(root="./data", train=True, download=True, transforms = transform)
+testset = datasets.CIFAR10(root="./data", train=False, download=True, transforms = transform)
 
 # Define the dataloader for train and test
-train = ...
-
-test = ...
+train_dataloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+test_dataloader = DataLoader(testset, batch_size=1, shuffle=True, num_workers=0)
 
--- training.py
+++ training.py
@@ -1,17 +1,59 @@
 import torch
 from torch.optim import Adam
+from torchvision import transforms
+
+from diffusion_model import Diffusion_model
+from dataloader import train_dataloader, test_dataloader
+
+from matplotlib import pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Model
+model = Diffusion_model()
+model.to(device)
+
 # Create loss function object
-criterion = ...
+criterion = ... # TODO: Define Loss function.
+
+# Learning Rate
+learning_rate = 1e-3
 
 # Optimizer function
-optimizer  = Adam(...)
+optimizer  = Adam(model.parameters(), lr=learning_rate, weight_decay=0.0005)
 
 # define training loop parameters
-num_epochs = ...
+num_epochs = 100
+
+# Gray scale Tranform
+gray_transform = transforms.Grayscale()
 
 # training loop
 for epoch in range(num_epochs):
-    pass
+    epoch_loss = 0
+    for _, data in enumerate(train_dataloader):
+        color_images, _ = data
+        gray_scale_images = gray_transform(color_images)
+
+        optimizer.zero_grad()
+        output = model(...) # TODO: Pass required input to the model and get output.
+
+        loss = criterion(...) # TODO: Add the required loss function with appropriate parameters.
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+    print("Epoch %d / %d and Loss: %d" % (epoch, num_epochs, epoch_loss))
+
+# Testing
+with torch.no_grad():
+    for _, data in enumerate(test_dataloader):
+        color_image, _ = data
+        gray_scale_image = gray_transform(color_image)
+
+        output = model(...) # TODO: Provide required input to the model.
+
+        # TODO: We can calculate the FID and IS scores here.
+
+        # TODO: Visualize the outputs and inputs.
