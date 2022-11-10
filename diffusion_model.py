ce0e10aa65f377674850ff98ccdd95b2087c5833
charan250498 <charan2598@gmail.com>
2022-11-09 23:05:55 -0800

    Added Sine Cosine Embedding.

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,4 +1,6 @@
+import torch
 from torch import nn
+import numpy as np
 
 class Block(nn.module):
     def __init__(self):
@@ -13,3 +15,15 @@ class Diffusion_model(nn.module):
 
     def forward(self):
         pass
+
+class Sine_Cosine_Embedding(nn.module):
+    def __init__(self, dim):
+        self.dim = dim
+        self.n = 10000
+    
+    def forward(self, timesteps):
+        # Expecting timesteps as [[0],[1],[2],[3],[4],....,[T]]
+        i = len(timesteps)
+        denominator = self.n**((2*i)//self.dim)
+        timesteps = timesteps/denominator
+        return torch.hstack(torch.sin(timesteps), torch.cos(timesteps))
