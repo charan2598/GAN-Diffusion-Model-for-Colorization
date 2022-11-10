2a2c4a74d5c5a11941615efa94c68fcf0240d8d1
mka219 <mka219@secb1013u-a02.csil.sfu.ca>
2022-11-10 12:48:29 -0800

    forward diffusion

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,7 +1,33 @@
 import torch
 from torch import nn
 import numpy as np
+class ForwardDiffusion():
 
+    def __init__(self, beta_start, beta_end, timesteps):
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.timesteps = timesteps
+        self.beta_schedule = self.linearBetaSchedule()
+        self.alpha = self.alphaGeneration()
+        self.alpha_bar = self.alphaBar()    
+        self.sqrt_alpha_bar = torch.sqrt(self.alpha_bar)
+        self.sqrt_one_minus_alpha_bar = torch.sqrt(1.0-self.alpha_bar)
+    
+    def alphaBar(self):
+        return torch.cumprod(self.alpha, dim=0)
+
+    def alphaGeneration(self):
+        return 1.0-self.beta_schedule
+    
+    def linearBetaSchedule(self):
+        return torch.linspace(self.beta_start, self.beta_end, self.timesteps)
+    
+    def forwardNoise(self, x, t):
+        noise = torch.randn_like(x.float())
+        alpha_root_bar =  torch.reshape(self.sqrt_alpha_bar[t], (-1, 1, 1, 1))
+        one_min_alpha_root_bar = torch.reshape(self.sqrt_one_minus_alpha_bar[t], (-1, 1, 1, 1))
+        noisy_image = ( alpha_root_bar * x ) + one_min_alpha_root_bar * noise
+        return noisy_image, noise
 class Block(nn.module):
     def __init__(self):
         pass
